433b43d2078db14bd9a381effbb703838503193d
Geovane Oliveira <geoliiveira2@gmail.com>
2021-04-27 00:14:09 -0300

    t_int_max* consertado

--- src/Esfera.cpp
+++ src/Esfera.cpp
@@ -2,33 +2,24 @@
 #include <iostream>
 
 bool Esfera::intersectar(const Raio& r,  float t_min, float t_max, PontoColisao& ptcol) const {
-    Ponto raiz_1, raiz_2;
-    double t_int_1, t_int_2;
     Vetor oc = r.origem() - _centro;
 
-    auto b = produto_escalar(oc, r.direcao());
-    auto c = produto_escalar(oc, oc)-(_raio*_raio); 
-    
-    auto delta = b*b - c;
+    float b = produto_escalar(oc, r.direcao());
+    float c = produto_escalar(oc, oc)-(_raio*_raio); 
+    float delta = b*b - c;
     
     if (delta < 0) return false;
     
-    t_int_1 = -b + sqrt(delta);
-    raiz_1 = r.para(t_int_1);
+    float t_int_mp = -b + sqrt(delta);
     
     if (delta > 0) {
-        t_int_2 = -b - sqrt(delta);
-        raiz_2 = r.para(t_int_2);
+        t_int_mp = -b - sqrt(delta);
         
-        if((r.origem()-raiz_1).comprimento() > (r.origem()-raiz_2).comprimento())
-        {
-            raiz_1 = raiz_2;
-            t_int_1 = t_int_2;
-        }
+        if(!(t_int_mp > t_min && t_int_mp < t_max)) return false;
     }
 
-    ptcol.t_int = t_int_1;
-    ptcol.pt = raiz_1;
+    ptcol.t_int = t_int_mp;
+    ptcol.pt = r.para(t_int_mp);
     ptcol.normal = (ptcol.pt - _centro) / _raio;
     ptcol.cor = _cor;
 
